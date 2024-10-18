ed1166de0fb407311891b211a19c9dcc933f2a03
Christopher Kelley <christopher@cktech.org>
2024-10-18 11:00:56 -0400

    Update Silent22H2.ps1
    Adjusted and added enablement package

--- Silent22H2.ps1
+++ Silent22H2.ps1
@@ -1,35 +1,25 @@
-### THIS SCRIPT WAS CREATED BY MARC-ANDRE TANGUAY, HEAD NERD @ N-ABLE
-### While N-able performs initial testing on these scripts, we do not regularly or permanently monitor these scripts, and therefore, we cannot make any guarantees about third-party content. By downloading or using any of these scripts, you agree that they are provided AS IS without warranty of any kind and we expressly disclaim all implied warranties including warranties of merchantability or of fitness for a particular purpose. In no event shall N-able or any other party be liable for any damages arising out of the use of or inability to use these scripts.
-### N-able suggests as a best practice that scripts should be tested on non-production environments.
-### UPDATED ON : MARCH 24 2023
-
+### Silent23H2 Upgrade
 
 $proceed=$false
-$osversion = Get-WMIObject win32_operatingsystem #| select Caption, Version
-$osbuild = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name DisplayVersion).DisplayVersion
-if($osversion.caption -like "*Windows 10*")
+$osversion = Get-WMIObject win32_operatingsystem
+$osbuild = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentBuild).CurrentBuild
+if($osversion.caption -like "*Windows 10*" -or $osversion.caption -like "*Windows 11*")
 {
-    write-host "Windows 10 detected"
-    "Windows 10 detected"
-    if($osbuild -eq "21H2" -or $osbuild -eq "2004" -or $osbuild -eq "22H1" -or $osbuild -eq "20H2"  -or $osbuild -eq "21H1")
+    "Windows 10 or Windows 11 detected"
+    if($osbuild -ge 19041)
     {
-        write-host "Build of Windows is compatible"
-         "Build of Windows is compatible"
+        "Build of Windows is compatible"
 
         $objSession = New-Object -com "Microsoft.Update.Session"
         $objSearcher = $objSession.CreateUpdateSearcher()
         $searchResult = $objSearcher.Search("IsInstalled=0 AND AutoSelectOnWebSites=0 or IsInstalled=0 AND AutoSelectOnWebSites=1 or IsInstalled=1 AND AutoSelectOnWebSites=0 or IsInstalled=1 AND AutoSelectOnWebSites=1 or IsInstalled=0 and DeploymentAction=* or IsInstalled=1 and DeploymentAction=*")
         $searchresult.Updates | ft 
         For ($i=0; $i -lt $searchresult.Updates.Count; $i++) {
-            $update = $searchResult.Updates.Item($i)    # IUpdate object, see WUA API
-            if($update.isinstalled -eq "True")
+            $update = $searchResult.Updates.Item($i)
+            if($update.isinstalled -eq "True" -and ($update.title.contains("Cumulative Update for Windows 10") -or $update.title.contains("Cumulative Update for Windows 11")))
             {
-                if($update.title.contains("2023-10 Cumulative Update for Windows 10") -or $update.title.contains("2023-09 Cumulative Update for Windows 10") -or $update.title.contains("2023-08 Cumulative Update for Windows 10") -or $update.title.contains("2023-07 Cumulative Update for Windows 10") -or $update.title.contains("2023-06 Cumulative Update for Windows 10") -or $update.title.contains("2023-05 Cumulative Update for Windows 10") -or $update.title.contains("2023-04 Cumulative Update for Windows 10") -or $update.title.contains("2023-03 Cumulative Update for Windows 10") -or $update.title.contains("2023-02 Cumulative Update for Windows 10") -or $update.title.contains("2023-01 Cumulative Update for Windows 10") -or $update.title.contains("2022-12 Cumulative Update for Windows 10") -or $update.title.contains("2022-11 Cumulative Update for Windows 10") -or $update.title.contains("2022-10 Cumulative Update for Windows 10") -or $update.title.contains("2022-09 Cumulative Update for Windows 10") -or $update.title.contains("2022-08 Cumulative Update for Windows 10") -or $update.title.contains("2023-10 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-09 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-08 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-07 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-06 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-05 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-04 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-03 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-02 Cumulative Update Preview for Windows 10") -or $update.title.contains("2023-01 Cumulative Update Preview for Windows 10") -or $update.title.contains("2022-12 Cumulative Update Preview for Windows 10") -or $update.title.contains("2022-11 Cumulative Update Preview for Windows 10") -or $update.title.contains("2022-10 Cumulative Update Preview for Windows 10") -or $update.title.contains("2022-09 Cumulative Update Preview for Windows 10") -or $update.title.contains("2022-08 Cumulative Update Preview for Windows 10"))
-                {
-                    write-host "found the required minimum cumulative update so all good - " + $update.title
-                     "found the required minimum cumulative update so all good - " + $update.title
-                    $proceed = $true
-                }
+                "found the required minimum cumulative update so all good - " + $update.title
+                $proceed = $true
             }
         }
         if($proceed -eq $false)
@@ -39,18 +29,15 @@ if($osversion.caption -like "*Windows 10*")
     }
     else
     {
-        write-host "Build of Windows is not compatible"
-         "Build of Windows is not compatible"
+        "Build of Windows is not compatible"
         $proceed = $False
     }
 }
 else 
 {
-    write-host "Windows 10 not detected"
-     "Windows 10 not detected"
+    "Windows 10 or Windows 11 not detected"
     $proceed=$False
 }
-
 
 if($proceed -eq $true)
 {
@@ -58,50 +45,52 @@ if($proceed -eq $true)
     $folderpresent = test-path c:\temp
     if($folderpresent -eq $False)
     {
-        write-host "Folder c:\temp didnt exist, creating it"
-         "Folder c:\temp didnt exist, creating it"
+        "Folder c:\temp didnt exist, creating it"
         md c:\temp >> $null
     }
 
     if([Environment]::Is64BitOperatingSystem -eq "True")
     {
-        Write-Host "64 bit Windows detected"
-         "64 bit Windows detected"
-        $url = ""
+        "64 bit Windows detected"
         $WebClient = New-Object System.Net.WebClient
-        $WebClient.DownloadFile("http://b1.download.windowsupdate.com/c/upgr/2022/07/windows10.0-kb5015684-x64_d2721bd1ef215f013063c416233e2343b93ab8c1.cab","C:\temp\windows10.0-kb5015684-x64_d2721bd1ef215f013063c416233e2343b93ab8c1.cab")
-        $updaterunArguments = '/online /Add-Package /PackagePath:"c:\temp\windows10.0-kb5015684-x64_d2721bd1ef215f013063c416233e2343b93ab8c1.cab" /quiet /norestart'
+        $updateUrl = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/updt/2023/10/windows11.0-kb5027397-x64_23h2.msu"
+        $updateFilePath = "C:\temp\windows11.0-kb5027397-x64_23H2.msu"
     }
     else 
     {
-        write-host "32 bit Windows detected"
-         "32 bit Windows detected"
-        $url = ""
-        $WebClient = New-Object System.Net.WebClient
-        $WebClient.DownloadFile("http://b1.download.windowsupdate.com/c/upgr/2022/07/windows10.0-kb5015684-x86_3734a3f6f4143b645788cc77154f6288c8054dd5.cab","C:\temp\windows10.0-kb5015684-x86_3734a3f6f4143b645788cc77154f6288c8054dd5.cab")
-        #C:\Windows\Sysnative\dism.exe  /Online /Add-Package /PackagePath:"windows10.0-kb5015684-x64_d2721bd1ef215f013063c416233e2343b93ab8c1.cab" /quiet /norestart"
-        $updaterunArguments = '/online /Add-Package /PackagePath:"C:\temp\windows10.0-kb5015684-x86_3734a3f6f4143b645788cc77154f6288c8054dd5.cab" /quiet /norestart'
+        "32 bit Windows detected"
+        "Error: Windows 11 23H2 is not supported for 32-bit systems."
+        exit 1
+    }
+
+    try
+    {
+        $WebClient.DownloadFile($updateUrl, $updateFilePath)
+    }
+    catch
+    {
+        "Error downloading the update file: $_"
+        exit 1
     }
     
-    $updaterunProcessCfg = New-Object System.Diagnostics.ProcessStartInfo
-    $updaterunProcessCfg.FileName = 'C:\Windows\system32\dism.exe'
-    $updaterunProcessCfg.RedirectStandardError = $true
-    $updaterunProcessCfg.RedirectStandardOutput = $true
-    $updaterunProcessCfg.UseShellExecute = $false
-    $updaterunProcessCfg.Arguments = $updaterunArguments
-    $updaterunProcess = New-Object System.Diagnostics.Process
-    $updaterunProcess.StartInfo = $updaterunProcessCfg
-    $updaterunProcess.Start() | Out-Null
-    $updaterunProcess.WaitForExit()
-    $updaterunProcessOutput = $updaterunProcess.StandardOutput.ReadToEnd()
-    $updaterunProcessErrors = $updaterunProcess.StandardError.ReadToEnd()
-    $updaterunProcessExitCode = $updaterunProcess.ExitCode
+    "Installing the update file"
+    $installerArguments = "/quiet /norestart"
+    
+    $installerProcessCfg = New-Object System.Diagnostics.ProcessStartInfo
+    $installerProcessCfg.FileName = 'wusa.exe'
+    $installerProcessCfg.RedirectStandardError = $true
+    $installerProcessCfg.RedirectStandardOutput = $true
+    $installerProcessCfg.UseShellExecute = $false
+    $installerProcessCfg.Arguments = "$updateFilePath $installerArguments"
+    $installerProcess = New-Object System.Diagnostics.Process
+    $installerProcess.StartInfo = $installerProcessCfg
+    $installerProcess.Start() | Out-Null
+    $installerProcess.WaitForExit()
+    $installerProcessOutput = $installerProcess.StandardOutput.ReadToEnd()
+    $installerProcessErrors = $installerProcess.StandardError.ReadToEnd()
+    $installerProcessExitCode = $installerProcess.ExitCode
 
-    "Execution Output : " + $updaterunProcessOutput
-    "Execution Errors : " + $updaterunProcessErrors
-    "Execution Exit Code : " + $updaterunProcessExitCode
-    write-host "Execution Output : " + $updaterunProcessOutput
-    write-host "Execution Errors : " + $updaterunProcessErrors
-    write-host "Execution Exit Code : " + $updaterunProcessExitCode
+    "Execution Output : " + $installerProcessOutput
+    "Execution Errors : " + $installerProcessErrors
+    "Execution Exit Code : " + $installerProcessExitCode
 }
-
